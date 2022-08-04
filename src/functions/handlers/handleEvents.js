f992c843263aef9658c439c0a15e9346c2a8569b
G-Linares <gerrylinares@gmail.com>
2022-08-04 17:37:48 -0500

    initial commit

---
+++ src/functions/handlers/handleEvents.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+module.exports = (client) => {
+    client.handleEvents = async () => {
+        const eventFolders = fs.readdirSync(`./src/events`);
+        for (const folder of eventFolders) {
+            const eventFiles = fs.readdirSync(`./src/event/${folder}`)
+        }
+    }
+}
